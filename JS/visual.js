43ad58fa9a53f95625f7007f392e3a1d4fdf1316
jjsena369 <jjsena369@gmail.com>
2025-10-11 10:25:10 -0500

    jaime

---
+++ JS/visual.js
@@ -0,0 +1,9 @@
+let squareOne = 12;
+let squareTwo = 10;
+let squareThree = 5;
+
+//PROCESO
+
+let areaOne = squareOne * squareOne;
+let areaTwo = squareTwo * squareTwo;
+let areaThree = squareThree * squareThree;  
