2673dc2b7f76748bbaf8dc053139f682deaecb03
jjsena369 <jjsena369@gmail.com>
2025-10-29 18:19:13 -0500

    Delete JS directory

--- JS/visual.js
+++
@@ -1,9 +0,0 @@
-let squareOne = 12;
-let squareTwo = 10;
-let squareThree = 5;
-
-//PROCESO
-
-let areaOne = squareOne * squareOne;
-let areaTwo = squareTwo * squareTwo;
-let areaThree = squareThree * squareThree;  
